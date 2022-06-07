f1465fc773cdd0856eaf62a1c4ada53577fd62c7
MEMiceli <marcello.emilio.miceli@gmail.com>
2022-06-07 16:21:10 +0200

    header fatto

---
+++ src/components/blocks/BaseHeader.vue
@@ -0,0 +1,29 @@
+<template>
+  <header>
+      <div class="image-logo">
+          <img src="../../assets/img/spotify-logo.png" alt="">
+      </div>
+  </header>
+</template>
+
+<script>
+export default {
+    name: 'BaseHeader',
+
+}
+</script>
+
+
+<style scoped lang="scss">
+    header{
+        background-color: var(--primary-color);
+    }
+    .image-logo{
+        padding: 1.25rem;
+        width: 6.25rem;
+        img{
+            width:100%;
+        }
+    }
+
+</style>
